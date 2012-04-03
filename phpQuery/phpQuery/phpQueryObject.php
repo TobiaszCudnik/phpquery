5db48a207a51d254226307ea87ce44d256272f9e
Kevin Miller <kemiller@csumb.edu>
2012-04-03 15:57:16 -0700

    Fixed specificity problem when later rules with the same specificity should override.o

--- phpQuery/phpQuery/phpQueryObject.php
+++ phpQuery/phpQuery/phpQueryObject.php
@@ -1433,7 +1433,7 @@ class phpQueryObject
 		      $existing = pq($el)->data('phpquery_css');
 		      $ruleset->expandShorthands();
 		      foreach($ruleset->getRules() as $rule => $value) {
-		        if(!isset($existing[$rule]) || $existing[$rule]['specificity'] < $specificity) {
+		        if(!isset($existing[$rule]) || $existing[$rule]['specificity'] <= $specificity) {
 		          $value = $value->getValue();
 		          $value = (is_object($value))
 		                    ? $value->__toString()
@@ -1453,7 +1453,7 @@ class phpQueryObject
 		  $style = strlen($style) ? explode(';', $style) : array();
 		  foreach($this->attribute_css_mapping as $map => $css_equivalent) {
 		    if($el->hasAttribute($map)) {
-		      $style[] = $css_equivalent .':'. pq($el)->attr($map) .';';
+		      $style[] = $css_equivalent .':'. pq($el)->attr($map);
 		    }
 		  }
 		  if(count($style)) {
@@ -1463,7 +1463,7 @@ class phpQueryObject
   		  $ruleset = reset($ruleset);
         $ruleset->expandShorthands();
         foreach($ruleset->getRules() as $rule => $value) {
-          if(!isset($existing[$rule]) || 1000 > $existing[$rule]['specificity']) {
+          if(!isset($existing[$rule]) || 1000 >= $existing[$rule]['specificity']) {
             $value = $value->getValue();
             $value = (is_object($value))
                       ? $value->__toString()
